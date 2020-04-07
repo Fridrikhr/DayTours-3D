752caf3a7be57d3cae3ab16355b287cf2211e64f
bjorgvinhall <43784017+bjorgvinhall@users.noreply.github.com>
2020-04-07 22:54:30 +0000

    Bætt við view og klösum fyrir Booking, hægt er að ýta á 'My bookings' og fara til baka

---
+++ src/Controller/BookingController.java
@@ -0,0 +1,44 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class BookingController {
+
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Text name11;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Text name;
+    @FXML
+    private Text name1;
+
+    @FXML
+    void backButtonHandler(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("./View/searchTours.fxml"));
+        Parent parent = loader.load();
+        Scene scene = new Scene(parent);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    void bookButtonHandler(ActionEvent event) {
+
+    }
+
+}

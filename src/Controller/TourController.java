fe74ad0c6a0d5e2629fff9a4adcfca2e981d8519
bjorgvinhall <43784017+bjorgvinhall@users.noreply.github.com>
2020-04-07 16:04:08 +0000

    Controller og view fyrir Tour

---
+++ src/Controller/TourController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TourController {
+
+    @FXML
+    private Button bookButton;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void bookButtonHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButtonHandler(ActionEvent event) {
+
+    }
+
+}
